94b4dff0413f07ec339e0573af60738b3dd524ba
sergiuchilat <chilatsergiu@gmail.com>
2023-02-13 13:52:00 +0200

    2 - Variables and constants

---
+++ 2 - Variables and constants/app.js
@@ -0,0 +1,18 @@
+console.log('Hello!')
+
+let someVariable = 123;
+const someConstant = 456;
+console.log(someVariable);
+console.log(someConstant);
+
+someVariable = 987;
+// someConstant = 789;
+
+console.log(someVariable);
+console.log(someConstant);
+
+
+let r = 1;
+const pi = 3.14;
+
+console.log(pi * r**2);
