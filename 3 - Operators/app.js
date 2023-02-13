3b03749e025c2374eae0eecd57164e1fc5a1b8e9
sergiuchilat <chilatsergiu@gmail.com>
2023-02-13 14:02:02 +0200

    3 - Operators

---
+++ 3 - Operators/app.js
@@ -0,0 +1,39 @@
+let x = 3;
+let y = 2;
+
+console.log('2 Numbers');
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+
+x = "2";
+y = "2";
+console.log('2 Strings');
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+x = 2;
+y = "2a";
+console.log('1 String + 1 Number');
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+let a = true;
+let b = false;
+
+console.log(a && a);
+console.log(a && b);
+console.log(a || a);
+console.log(a || a);
+console.log(b || b);
+console.log(!a);
+console.log(!b);
